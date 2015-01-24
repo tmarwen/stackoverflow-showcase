3df48d918a836904ec8b72df17a3f8b7ca6487ca
tmarwen <marwen.trabelsi.insat@gmail.com>
2015-01-24 15:47:52 +0100

    [FIX] Parent and Child Maven modules unified

---
+++ jframe-calculator/src/main/java/com/github/tmawen/stackoverflow/Operation.java
@@ -0,0 +1,11 @@
+package com.github.tmawen.stackoverflow;
+
+/**
+ * Operation
+ *
+ * @author <a href="mailto:marwen.trabelsi.insat@gmail.com">Marwen Trabelsi</a>
+ * @version 1.0
+ */
+public enum Operation
+{
+}
