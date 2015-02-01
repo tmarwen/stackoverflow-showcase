e1b21249cd8bc06e0d6002accc9284cb9b70f9f1
tmarwen <marwen.trabelsi.insat@gmail.com>
2015-02-01 14:07:27 +0100

    [FTR] Package WAR module with sources inside /lib directory

---
+++ war-with-sources/src/main/java/com/github/tmarwen/stackoverflow/showcase/Test.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2015 Wise Brains
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.github.tmarwen.stackoverflow.showcase;
+
+/**
+ * Test
+ *
+ * @author <a href="mailto:marwen.trabelsi.insat@gmail.com">Marwen Trabelsi</a>
+ * @version 1.0
+ */
+public class Test
+{
+  public String callMe(String arg)
+  {
+    return arg;
+  }
+}
