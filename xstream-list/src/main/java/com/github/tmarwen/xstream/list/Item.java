c6e35cd2cc32b4fe45926e971b1b434af9d8a780
tmarwen <marwen.trabelsi.insat@gmail.com>
2015-05-24 16:09:52 +0100

    [FTR] Changing java.util.List item name in XML with XStream

---
+++ xstream-list/src/main/java/com/github/tmarwen/xstream/list/Item.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2015 Wise Brains
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.github.tmarwen.xstream.list;
+
+/**
+ * Item
+ *
+ * @author <a href="mailto:marwen.trabelsi.insat@gmail.com">Marwen Trabelsi</a>
+ * @version 1.0
+ */
+public class Item
+{
+    private int order;
+    private String name;
+
+    public Item( int order, String name )
+    {
+        this.order = order;
+        this.name = name;
+    }
+
+    public int getOrder()
+    {
+        return order;
+    }
+
+    public void setOrder( int order )
+    {
+        this.order = order;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+}
---
+++ xstream-list/src/main/java/com/github/tmarwen/xstream/list/MainSerialization.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2015 Wise Brains
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.github.tmarwen.xstream.list;
+
+import com.thoughtworks.xstream.XStream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * MainSerizalization
+ *
+ * @author <a href="mailto:marwen.trabelsi.insat@gmail.com">Marwen Trabelsi</a>
+ * @version 1.0
+ */
+public class MainSerialization
+{
+    public static void main( String[] args )
+    {
+        Item itemOne = new Item( 0, "item-1" );
+        Item itemTwo = new Item( 1, "item-2" );
+
+        List<Item> items = new ArrayList< Item >();
+        items.add( itemOne );
+        items.add( itemTwo );
+
+        Package pakage = new Package();
+        pakage.setItems( items );
+
+        XStream xstream = new XStream();
+        xstream.alias( "package", Package.class );
+        xstream.processAnnotations( Package.class );
+        String abc = xstream.toXML( pakage );
+        System.out.println( abc );
+    }
+}
